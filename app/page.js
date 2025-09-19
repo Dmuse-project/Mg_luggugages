7ce6f7afd50ab743c3c03d8b1101ad621f759af3
Dmuse-project <danieluche702@gmail.com>
2025-09-19 16:37:01 +0100

    component position update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import Hero from "../components/hero/hero";
-import Features from "../components/features/features";
-import Products from "../components/products/products";
-import HowItWorks from "../components/howItWorks/howItWorks";
-import CTA from "../components/cta/cta";
-import Footer from "../components/footer/footer";
-import Navbar from "@/components/nav/nav";
+import Hero from "./components/hero/hero";
+import Features from "./components/features/features";
+import Products from "./components/products/products";
+import HowItWorks from "./components/howItWorks/howItWorks";
+import CTA from "./components/cta/cta";
+import Footer from "./components/footer/footer";
+import Navbar from "@/app/components/nav/nav";
 
 export default function Home() {
   return (
